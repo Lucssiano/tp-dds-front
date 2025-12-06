98c76c2b5caedf949cecaf394cbba7421c076682
MatyTort01 <mtort@frba.utn,edu.ar>
2025-12-05 23:29:39 -0300

    Saco import.

--- src/main/java/ar/utn/ba/dds/front_tp/controller/HechosController.java
+++ src/main/java/ar/utn/ba/dds/front_tp/controller/HechosController.java
@@ -1,7 +1,6 @@
 package ar.utn.ba.dds.front_tp.controller;
 
 import ar.utn.ba.dds.front_tp.Utils.JwtUtils;
-import ar.utn.ba.dds.front_tp.dto.colecciones.ColeccionDTO;
 import ar.utn.ba.dds.front_tp.dto.hechos.CategoriaDTO;
 import ar.utn.ba.dds.front_tp.dto.hechos.CrearHechoDTO;
 import ar.utn.ba.dds.front_tp.dto.input.ColeccionInputDTO;
